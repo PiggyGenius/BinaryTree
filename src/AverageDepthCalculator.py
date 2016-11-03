2d4fa7079f314cb0e6dc1593b99d2a6d57343639
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2016-11-03 19:02:01 +0100

    Prepared algorithm for memoization

--- src/AverageDepthCalculator.py
+++ src/AverageDepthCalculator.py
@@ -12,34 +12,31 @@ class AverageDepthCalculator(object):
 
     def average_depth(self, proba):
         self.proba = proba
-        self.root, self.avg_depth = self.average_depth_rec(0, len(proba), 1);
+        self.avg_depth = self.average_depth_rec(0, len(proba), 1);
+        self.root = Node(0)
 
     def average_depth_rec(self, i_low, i_high, depth):
         self.call_count += 1
         diff = i_high - i_low
         if diff == 1:
-            return Node(i_low), self.proba[i_low]*depth
+            return self.proba[i_low]*depth
         if diff == 0:
-            return None, 0
+            return 0
         if diff < 0:
             raise ValueError("BASTARD")
 
-        best_node_low, low = self.average_depth_rec(i_low, i_low, depth+1)
-        best_node_high, high = self.average_depth_rec(i_low+1, i_high, depth+1)
+        low = self.average_depth_rec(i_low, i_low, depth+1)
+        high = self.average_depth_rec(i_low+1, i_high, depth+1)
         min_index = i_low
         min_val = self.proba[i_low]*depth + low + high
         
-        for i in range(i_low+1, i_high):
-            node_low, low = self.average_depth_rec(i_low, i, depth+1) 
-            node_high, high = self.average_depth_rec(i+1, i_high, depth+1)
+        for i in range(i_low, i_high):
+            low = self.average_depth_rec(i_low, i, depth+1) 
+            high = self.average_depth_rec(i+1, i_high, depth+1)
             val = self.proba[i]*depth + low + high
             if val < min_val:
                 min_val = val
                 min_index = i
-                best_node_low = node_low
-                best_node_high = node_high
 
-        node = Node(min_index)
-        node.add_child(LEFT, best_node_low)
-        node.add_child(RIGHT, best_node_high)
-        return node, min_val
+        # memoiser min_index et min_val dans [i_low, i_high]
+        return min_val
