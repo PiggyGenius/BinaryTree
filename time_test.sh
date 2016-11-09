e2da57f926c444ac32fbdf96da914f1f61ac1d83
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2016-11-09 22:20:37 +0100

    Added time measure script

---
+++ time_test.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+make binary
+
+b=1
+for length in 5 10 1000 2000 3000 5000
+do
+	echo "========== Benchmark $b =========="
+	for i in `seq 1 5`
+	do
+		time ./bin/computeABROpt $length benchmarks/benchmark$b.in > /dev/null
+		echo "----------------------------------"
+	done
+	((b++))
+done
